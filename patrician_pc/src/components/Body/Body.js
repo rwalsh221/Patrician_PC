f73634205b34efd84dfc10af2cc5d4b55e58540f
Richard Walsh <richwalsh2@googlemail.com>
2021-05-22 20:05:44 +0100

    add all props to items. add default data obj to item

--- patrician_pc/src/components/Body/Body.js
+++ patrician_pc/src/components/Body/Body.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 // import { database } from '../../firebase';
 
 import Item from '../Item/Item';
@@ -6,13 +6,13 @@ import Item from '../Item/Item';
 import classes from './Body.module.css';
 
 const Body = () => {
-  const [addPrice, setAddprice] = useState(false);
+  const [itemPrices, setItemPrices] = useState(null);
   console.log('render');
 
   const database =
     'https://patrician3-pc-default-rtdb.europe-west1.firebasedatabase.app/';
 
-  const postConfig = {
+  const patchConfig = {
     method: 'PATCH',
     headers: {
       Accept: 'application/json',
@@ -20,30 +20,51 @@ const Body = () => {
     },
   };
 
+  const getItemPrices = async () => {
+    try {
+      const getPrices = await fetch(`${database}/item.json`);
+
+      const getPricesJson = await getPrices.json();
+
+      setItemPrices(getPricesJson);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  console.log(itemPrices);
+  useEffect(() => {
+    getItemPrices();
+  }, []);
+
   const submitPriceHandler = async (name, newPrice, alert) => {
-    // TODO: need to save prices as array
+    // REMOVE SPACES FROM NAME
+    if (name.includes(' ')) name = name.split(' ').join('');
+
     try {
       if (newPrice <= 0) return alert('PRICE MUST BE GREATER THAN ZERO');
       // 1 get prices array from db
       const getPrices = await fetch(`${database}/item/${name}.json`);
 
       let getPricesJson = await getPrices.json();
-      if (getPricesJson === null) getPricesJson = [];
-      console.log(getPricesJson);
+      if (getPricesJson === null)
+        getPricesJson = {
+          allPrice: [],
+          hiPrice: 0,
+          loPrice: 0,
+          avgPrice: 0,
+        };
 
-      // 2 add new price to array
+      // 2 add new price to allprice array
       getPricesJson.allPrice.push(+newPrice);
-      console.log(getPricesJson);
 
-      // 3 sort hi lo avg
+      // 3 get hi lo avg
       getPricesJson.allPrice.sort(function (a, b) {
         return a - b;
       });
-      console.log(getPricesJson);
+
       const hiPrice = getPricesJson.allPrice[getPricesJson.allPrice.length - 1];
-      console.log(hiPrice);
+
       const loPrice = getPricesJson.allPrice[0];
-      console.log(loPrice);
 
       let sum = 0;
 
@@ -52,11 +73,11 @@ const Body = () => {
       }
 
       const avgPrice = sum / getPricesJson.allPrice.length;
-      console.log(avgPrice.toFixed());
+
       // 4 send back to db
 
       const addPrice = await fetch(`${database}/item/${name}.json`, {
-        ...postConfig,
+        ...patchConfig,
         body: JSON.stringify({
           allPrice: [...getPricesJson.allPrice],
           hiPrice: hiPrice,
@@ -66,7 +87,7 @@ const Body = () => {
       });
 
       if (addPrice.ok) {
-        setAddprice(true);
+        await getItemPrices();
         alert();
       }
       if (!addPrice.ok) throw new Error();
@@ -74,6 +95,7 @@ const Body = () => {
       console.error(err.message);
     }
   };
+
   return (
     <main>
       <div className={classes.scrollContainer}>
@@ -83,29 +105,103 @@ const Body = () => {
           <Item
             name={'beer'}
             priceHandler={submitPriceHandler}
-            hi={20}
-            lo={10}
-            avg={15}
+            data={itemPrices}
           />
-          <Item name={'bricks'} hi={20} lo={10} avg={15} />
-          <Item name={'cloth'} hi={20} lo={10} avg={15} />
-          <Item name={'fish'} hi={20} lo={10} avg={15} />
-          <Item name={'grain'} hi={20} lo={10} avg={15} />
-          <Item name={'hemp'} hi={20} lo={10} avg={15} />
-          <Item name={'honey'} hi={20} lo={10} avg={15} />
-          <Item name={'iron goods'} hi={20} lo={10} avg={15} />
-          <Item name={'leather'} hi={20} lo={10} avg={15} />
-          <Item name={'meat'} hi={20} lo={10} avg={15} />
-          <Item name={'pig iron'} hi={20} lo={10} avg={15} />
-          <Item name={'pitch'} hi={20} lo={10} avg={15} />
-          <Item name={'pottery'} hi={20} lo={10} avg={15} />
-          <Item name={'salt'} hi={20} lo={10} avg={15} />
-          <Item name={'skins'} hi={20} lo={10} avg={15} />
-          <Item name={'spices'} hi={20} lo={10} avg={15} />
-          <Item name={'timber'} hi={20} lo={10} avg={15} />
-          <Item name={'whale oil'} hi={20} lo={10} avg={15} />
-          <Item name={'wine'} hi={20} lo={10} avg={15} />
-          <Item name={'wool'} hi={20} lo={10} avg={15} />
+          <Item
+            name={'bricks'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
+          <Item
+            name={'cloth'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
+          <Item
+            name={'fish'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
+          <Item
+            name={'grain'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
+          <Item
+            name={'hemp'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
+          <Item
+            name={'honey'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
+          <Item
+            name={'iron goods'}
+            priceHandler={submitPriceHandler}
+            data={itemPrices}
+          />
+          <Item
+            name={'leather'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
+          <Item
+            name={'meat'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
+          <Item
+            name={'pig iron'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
+          <Item
+            name={'pitch'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
+          <Item
+            name={'pottery'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
+          <Item
+            name={'salt'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
+          <Item
+            name={'skins'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
+          <Item
+            name={'spices'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
+          <Item
+            name={'timber'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
+          <Item
+            name={'whale oil'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
+          <Item
+            name={'wine'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
+          <Item
+            name={'wool'}
+            data={itemPrices}
+            priceHandler={submitPriceHandler}
+          />
         </div>
         <div className={classes.scrollBottom}></div>
       </div>
--- patrician_pc/src/components/Item/Item.js
+++ patrician_pc/src/components/Item/Item.js
@@ -16,18 +16,42 @@ const Item = (props) => {
     }, 10000);
   };
 
+  const dataKey = props.name.split(' ').join('');
+  let data = { ...props.data };
+
+  if (data && data[dataKey] === undefined)
+    data = {
+      ...data,
+      [dataKey]: { avgPrice: 0, loPrice: 0, hiPrice: 0 },
+    };
+
   return (
     <div className={classes.itemContainer}>
       <h2 className={classes.name}>{props.name}:</h2>
       <ul className={classes.priceList}>
         <li className={classes.priceLabel}>
-          AVG:{<span className={classes.priceProp}>{props.avg}</span>}
+          AVG:
+          {
+            <span className={classes.priceProp}>
+              {data ? data[dataKey].avgPrice : 0}
+            </span>
+          }
         </li>
         <li className={classes.priceLabel}>
-          LO:{<span className={classes.priceProp}>{props.lo}</span>}
+          LO:
+          {
+            <span className={classes.priceProp}>
+              {data ? data[dataKey].loPrice : 0}
+            </span>
+          }
         </li>
         <li className={classes.priceLabel}>
-          HI:{<span className={classes.priceProp}>{props.hi}</span>}
+          HI:
+          {
+            <span className={classes.priceProp}>
+              {data ? data[dataKey].hiPrice : 0}
+            </span>
+          }
         </li>
       </ul>
       <form className={classes.form}>
@@ -46,7 +70,7 @@ const Item = (props) => {
               inputRef.current.value,
               showAlert
             );
-            // showAlert();
+            inputRef.current.value = '';
           }}
         >
           Submit Price
