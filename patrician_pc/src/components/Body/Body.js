dba79a56ee4de6479b6d261d6415370d88d14b95
Richard Walsh <richwalsh2@googlemail.com>
2021-05-28 20:05:04 +0100

    add sell prices. change DB structure. change css and responsiiveness

--- patrician_pc/src/components/Body/Body.js
+++ patrician_pc/src/components/Body/Body.js
@@ -5,6 +5,8 @@ import Item from '../Item/Item';
 
 import classes from './Body.module.css';
 
+// TODO: COULD ADD POPUP TO SEE ALL PRICES AND DELETE
+
 const Body = () => {
   const [itemPrices, setItemPrices] = useState(null);
 
@@ -25,6 +27,92 @@ const Body = () => {
 
       const getPricesJson = await getPrices.json();
 
+      // const prices = {
+      //   buy: { ...getBuyPricesJson },
+      //   sell: {
+      //     beer: {
+      //       allPrice: [10, 30, 32, 40, 43, 46, 121],
+      //       avgPrice: 46,
+      //       hiPrice: 121,
+      //       loPrice: 10,
+      //     },
+      //     cloth: {
+      //       allPrice: [204, 306],
+      //       avgPrice: 255,
+      //       hiPrice: 306,
+      //       loPrice: 204,
+      //     },
+      //     fish: {
+      //       allPrice: [433],
+      //       avgPrice: 433,
+      //       hiPrice: 433,
+      //       loPrice: 433,
+      //     },
+      //     grain: {
+      //       allPrice: [89],
+      //       avgPrice: 89,
+      //       hiPrice: 89,
+      //       loPrice: 89,
+      //     },
+      //     hemp: {
+      //       allPrice: [264],
+      //       avgPrice: 264,
+      //       hiPrice: 264,
+      //       loPrice: 264,
+      //     },
+      //     irongoods: {
+      //       allPrice: [242, 253, 253],
+      //       avgPrice: 249,
+      //       hiPrice: 253,
+      //       loPrice: 242,
+      //     },
+      //     leather: {
+      //       allPrice: [235, 268],
+      //       avgPrice: 252,
+      //       hiPrice: 268,
+      //       loPrice: 235,
+      //     },
+      //     meat: {
+      //       allPrice: [938, 1110],
+      //       avgPrice: 1024,
+      //       hiPrice: 1110,
+      //       loPrice: 938,
+      //     },
+      //     pigiron: {
+      //       allPrice: [696, 776, 1012, 2927],
+      //       avgPrice: 1353,
+      //       hiPrice: 2927,
+      //       loPrice: 696,
+      //     },
+      //     salt: {
+      //       allPrice: [31],
+      //       avgPrice: 31,
+      //       hiPrice: 31,
+      //       loPrice: 31,
+      //     },
+      //     skins: {
+      //       allPrice: [829],
+      //       avgPrice: 829,
+      //       hiPrice: 829,
+      //       loPrice: 829,
+      //     },
+      //     timber: {
+      //       allPrice: [35],
+      //       avgPrice: 35,
+      //       hiPrice: 35,
+      //       loPrice: 35,
+      //     },
+      //     wine: {
+      //       allPrice: [167],
+      //       avgPrice: 167,
+      //       hiPrice: 167,
+      //       loPrice: 167,
+      //     },
+      //   },
+      // };
+
+      console.log(getPricesJson);
+
       setItemPrices(getPricesJson);
     } catch (error) {
       console.error(error);
@@ -35,7 +123,9 @@ const Body = () => {
     getItemPrices();
   }, []);
 
-  const submitPriceHandler = async (name, newPrice, alert) => {
+  console.log(itemPrices);
+
+  const submitPriceHandler = async (name, newPrice, buySell, alert) => {
     // REMOVE SPACES FROM NAME
     if (name.includes(' ')) name = name.split(' ').join('');
 
@@ -45,40 +135,41 @@ const Body = () => {
       const getPrices = await fetch(`${database}/item/${name}.json`);
 
       let getPricesJson = await getPrices.json();
-      if (getPricesJson === null)
+      if (getPricesJson === null || getPricesJson[buySell] === undefined)
         getPricesJson = {
-          allPrice: [],
-          hiPrice: 0,
-          loPrice: 0,
-          avgPrice: 0,
+          [buySell]: { allPrice: [], hiPrice: 0, loPrice: 0, avgPrice: 0 },
+          // sell: { allPrice: [], hiPrice: 0, loPrice: 0, avgPrice: 0 },
         };
 
       // 2 add new price to allprice array
-      getPricesJson.allPrice.push(+newPrice);
+      getPricesJson[buySell].allPrice.push(+newPrice);
 
       // 3 get hi lo avg
-      getPricesJson.allPrice.sort(function (a, b) {
+      getPricesJson[buySell].allPrice.sort(function (a, b) {
         return a - b;
       });
 
-      const hiPrice = getPricesJson.allPrice[getPricesJson.allPrice.length - 1];
+      const hiPrice =
+        getPricesJson[buySell].allPrice[
+          getPricesJson[buySell].allPrice.length - 1
+        ];
 
-      const loPrice = getPricesJson.allPrice[0];
+      const loPrice = getPricesJson[buySell].allPrice[0];
 
       let sum = 0;
 
-      for (let i = 0; i < getPricesJson.allPrice.length; i++) {
-        sum += getPricesJson.allPrice[i];
+      for (let i = 0; i < getPricesJson[buySell].allPrice.length; i++) {
+        sum += getPricesJson[buySell].allPrice[i];
       }
 
-      const avgPrice = sum / getPricesJson.allPrice.length;
+      const avgPrice = sum / getPricesJson[buySell].allPrice.length;
 
       // 4 send back to db
 
-      const addPrice = await fetch(`${database}/item/${name}.json`, {
+      const addPrice = await fetch(`${database}/item/${name}/${buySell}.json`, {
         ...patchConfig,
         body: JSON.stringify({
-          allPrice: [...getPricesJson.allPrice],
+          allPrice: [...getPricesJson[buySell].allPrice],
           hiPrice: hiPrice,
           loPrice: loPrice,
           avgPrice: +avgPrice.toFixed(),
--- patrician_pc/src/components/Item/Item.js
+++ patrician_pc/src/components/Item/Item.js
@@ -9,82 +9,153 @@ const Item = (props) => {
     className: 'initial',
   });
 
-  const inputRef = useRef();
+  const buyInputRef = useRef();
+  const sellInputRef = useRef();
 
+  // FOR ALERT MESSAGE ON BTN
   const showAlert = (message = 'SUCCESS', className = 'success') => {
     let alert = { ...alertMessage };
 
     alert = { alertMessage: message, disabled: true, className: className };
     setAlertMessage({ ...alert });
+
     setTimeout(() => {
       alert = { alertMessage: null, disabled: false, className: 'initial' };
       setAlertMessage({ ...alert });
-    }, 5000);
+    }, 2000);
   };
 
+  // SETS DATAKEY AND REMOVE SPACE i.e 'PIG IRON'
   const dataKey = props.name.split(' ').join('');
   let data = { ...props.data };
 
-  if (data && data[dataKey] === undefined)
+  // CREATE DATA OBJ IF ITEM DOESNT EXIST IN DATABASE
+  // BUYOBJ
+  if (data[dataKey] === undefined || data[dataKey].buy === undefined) {
     data = {
-      ...data,
-      [dataKey]: { avgPrice: 0, loPrice: 0, hiPrice: 0 },
+      [dataKey]: {
+        ...data[dataKey],
+        buy: { avgPrice: 0, loPrice: 0, hiPrice: 0 },
+      },
     };
+  }
+  // SELL OBJ
+  if (data[dataKey] === undefined || data[dataKey].sell === undefined) {
+    data = {
+      [dataKey]: {
+        ...data[dataKey],
+        sell: { avgPrice: 0, loPrice: 0, hiPrice: 0 },
+      },
+    };
+  }
+
+  const submitPriceHandler = async () => {
+    if (buyInputRef.current.value !== '') {
+      await props.priceHandler(
+        props.name,
+        buyInputRef.current.value,
+        'buy',
+        showAlert
+      );
+      buyInputRef.current.value = '';
+    } else if (sellInputRef.current.value !== '') {
+      await props.priceHandler(
+        props.name,
+        sellInputRef.current.value,
+        'sell',
+        showAlert
+      );
+      sellInputRef.current.value = '';
+    }
+  };
 
   return (
     <div className={classes.itemContainer}>
       <h2 className={classes.name}>{props.name}:</h2>
-      <ul className={classes.priceList}>
-        <li className={classes.priceLabel}>
-          AVG:
-          {
-            <span className={classes.priceProp}>
-              {data ? data[dataKey].avgPrice : 0}
-            </span>
-          }
-        </li>
-        <li className={classes.priceLabel}>
-          LO:
-          {
-            <span className={classes.priceProp}>
-              {data ? data[dataKey].loPrice : 0}
-            </span>
-          }
-        </li>
-        <li className={classes.priceLabel}>
-          HI:
-          {
-            <span className={classes.priceProp}>
-              {data ? data[dataKey].hiPrice : 0}
-            </span>
-          }
-        </li>
-      </ul>
-      <form className={classes.form}>
+      <div className={classes.buy}>
+        <h3>BUY:</h3>
+        <ul className={classes.priceList}>
+          <li className={classes.priceLabel}>
+            AVG:
+            {
+              <span className={classes.priceProp}>
+                {data ? data[dataKey].buy.avgPrice : 0}
+              </span>
+            }
+          </li>
+          <li className={classes.priceLabel}>
+            LO:
+            {
+              <span className={classes.priceProp}>
+                {data ? data[dataKey].buy.loPrice : 0}
+              </span>
+            }
+          </li>
+          <li className={classes.priceLabel}>
+            HI:
+            {
+              <span className={classes.priceProp}>
+                {data ? data[dataKey].buy.hiPrice : 0}
+              </span>
+            }
+          </li>
+        </ul>
+      </div>
+      <div className={classes.sell}>
+        <h3>SELL:</h3>
+        <ul className={classes.priceList}>
+          <li className={classes.priceLabel}>
+            AVG:
+            {
+              <span className={classes.priceProp}>
+                {data ? data[dataKey].sell.avgPrice : 0}
+              </span>
+            }
+          </li>
+          <li className={classes.priceLabel}>
+            LO:
+            {
+              <span className={classes.priceProp}>
+                {data ? data[dataKey].sell.loPrice : 0}
+              </span>
+            }
+          </li>
+          <li className={classes.priceLabel}>
+            HI:
+            {
+              <span className={classes.priceProp}>
+                {data ? data[dataKey].sell.hiPrice : 0}
+              </span>
+            }
+          </li>
+        </ul>
+      </div>
+      <form className={classes.buyForm}>
         <input
           type={'number'}
-          name={'price'}
-          placeholder={'Input New Price Above Zero'}
-          ref={inputRef}
+          name={'buy_price'}
+          placeholder={'BUY'}
+          ref={buyInputRef}
         />
-        <button
-          disabled={alertMessage.disabled}
-          className={`${classes.submitBtn} ${classes[alertMessage.className]}`}
-          onClick={async (e) => {
-            e.preventDefault();
-            await props.priceHandler(
-              props.name,
-              inputRef.current.value,
-              showAlert
-            );
-            inputRef.current.value = '';
-          }}
-        >
-          {alertMessage.alertMessage
-            ? alertMessage.alertMessage
-            : 'Submit Price'}
-        </button>
       </form>
+      <form className={classes.sellForm}>
+        <input
+          type={'number'}
+          name={'sell_price'}
+          placeholder={'SELL'}
+          ref={sellInputRef}
+        />
+      </form>
+      <button
+        disabled={alertMessage.disabled}
+        className={`${classes.submitBtn} ${classes[alertMessage.className]}`}
+        onClick={async (e) => {
+          e.preventDefault();
+          await submitPriceHandler();
+        }}
+      >
+        {alertMessage.alertMessage ? alertMessage.alertMessage : 'Submit Price'}
+      </button>
     </div>
   );
 };
