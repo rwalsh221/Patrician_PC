47043a377eb380318c2c8cafa1cc8debfc059aab
Richard Walsh <richwalsh2@googlemail.com>
2021-08-19 18:55:37 +0100

    remove unused footer. refactor header

--- patrician_pc/src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import classes from './Footer.module.css';
-
-const Footer = () => {
-  return <footer>THIS IS A TEST FOOTER</footer>;
-};
-
-export default Footer;
--- patrician_pc/src/components/Header/Header.js
+++ patrician_pc/src/components/Header/Header.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 import classes from './Header.module.css';
 
-const Header = () => {
-  return (
-    <header className={classes.header}>
-      <h1 className={classes.mainHeading}>PATRICIAN 3 PRICE CHECKER</h1>
-    </header>
-  );
-};
+const Header = () => (
+  <header className={classes.header}>
+    <h1 className={classes.mainHeading}>PATRICIAN 3 PRICE CHECKER</h1>
+  </header>
+);
 
 export default Header;
