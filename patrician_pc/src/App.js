eeff26c11f44b18d224247cd7a1a3fd9fc84d802
Richard Walsh <richwalsh2@googlemail.com>
2021-08-19 19:20:28 +0100

    delete body component

--- patrician_pc/src/App.js
+++ patrician_pc/src/App.js
@@ -3,18 +3,8 @@ import React from 'react';
 import Main from './layout/Main/Main';
 
 require('./App.css');
-// require('./Variables.css');
-// require('./Grids.css');
-
-let x;
 
 function App() {
-  for (let i = 0; i < 100; i += 1) {
-    console.log(i);
-    console.log(i + 10);
-    console.log((i += 10));
-    i += 10;
-  }
   return (
     <div className="App">
       <Main />
