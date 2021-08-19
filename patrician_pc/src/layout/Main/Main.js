d377e9d9dbbcd9d469f7fd19c2c9523d745642b4
Richard Walsh <richwalsh2@googlemail.com>
2021-08-19 19:16:53 +0100

    refactor Main.js eslint

--- patrician_pc/src/layout/Main/Main.js
+++ patrician_pc/src/layout/Main/Main.js
@@ -1,30 +1,17 @@
 import React from 'react';
 
-// import Header from '../../components/Header/Header';
-import Body from '../../components/Body/Body';
 import ScrollCenter from '../../components/ScrollCenter/ScrollCenter';
 import ScrollEdge from '../../components/ScrollEdge/ScrollEdge';
-// import Footer from '../../components/Footer/Footer';
 
 import classes from './Main.module.css';
 
-const Main = () => {
-  return (
-    <main className={classes.main}>
-      <section className={classes.scrollContainer}>
-        <ScrollEdge position={'scrollTop'} />
-        <ScrollCenter />
-        <ScrollEdge position={'scrollBottom'} />
-      </section>
-    </main>
-  );
-  // return (
-  //   <div className={classes.main}>
-  //     {/* <Header /> */}
-  //     <Body />
-  //     {/* <Footer /> */}
-  //   </div>
-  // );
-};
-
+const Main = () => (
+  <main className={classes.main}>
+    <section className={classes.scrollContainer}>
+      <ScrollEdge position="scrollTop" />
+      <ScrollCenter />
+      <ScrollEdge position="scrollBottom" />
+    </section>
+  </main>
+);
 export default Main;
